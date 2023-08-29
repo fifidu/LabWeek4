cf3502c361925dab057d85d1d859eaaadb9e2377
fifidu <fifi.du@student.uts.edu.au>
2023-08-29 10:27:29 +1000

    90 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ConsolePrint : MonoBehaviour
 {
     private int i;
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
